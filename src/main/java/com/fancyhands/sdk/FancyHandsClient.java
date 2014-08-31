7cb9712c463ade5d09ec90bdc29bc3887732dd6e
Ted Roden <tedroden@gmail.com>
2014-08-30 21:35:26 -0400

    let's get the url right this timne.

--- src/main/java/com/fancyhands/sdk/FancyHandsClient.java
+++ src/main/java/com/fancyhands/sdk/FancyHandsClient.java
@@ -25,7 +25,7 @@ import org.json.JSONObject;
  * Created by troden on 8/21/14.
  */
 public class FancyHandsClient    {
-    private String API_BASE = "http://localhost:8080/api/v1/";
+    private String API_BASE = "https://www.fancyhands.com/api/v1/";
 
     protected String API_KEY = null;
     protected String API_SECRET = null;
